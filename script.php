1634a1a00f2cd487cc934bf98f593c95be72330b
danilgu2 <danilgu@yandex.ru>
2014-10-25 16:04:49 +0400

    Update script.php

--- script.php
+++ script.php
@@ -1,8 +1,3 @@
-<?php
-import fl.display.ProLoader;
-var fl_ProLoader_2:ProLoader;
-		fl_ProLoader_2 = new ProLoader();
-		fl_ProLoader_2.load(new URLRequest("http://www.helpexamples.com/flash/images/image1.jpg"));
-		addChild(fl_ProLoader_2);
-	
+<?
+echo 'http://flapps.ru';
 ?>
