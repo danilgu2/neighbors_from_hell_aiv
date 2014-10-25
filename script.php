cba9ccddff19a61421a0e7163c2637a8b997d808
danilgu2 <danilgu@yandex.ru>
2014-10-25 15:59:30 +0400

    Update script.php

--- script.php
+++ script.php
@@ -1,3 +1,8 @@
 <?php
-var myText.text = "myMessgae";
+import fl.display.ProLoader;
+var fl_ProLoader_2:ProLoader;
+		fl_ProLoader_2 = new ProLoader();
+		fl_ProLoader_2.load(new URLRequest("http://www.helpexamples.com/flash/images/image1.jpg"));
+		addChild(fl_ProLoader_2);
+	
 ?>
